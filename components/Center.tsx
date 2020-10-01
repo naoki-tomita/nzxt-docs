0466d8ed467f99b2e4d83347c4270d8b63b43f5b
kojiro.ueda <mxcpy608@hotmail.com>
2020-10-01 15:08:03 +0900

    Improve top

--- components/Center.tsx
+++ components/Center.tsx
@@ -1,5 +1,5 @@
 import { h, Component } from "zheleznaya";
 
 export const Center: Component = (_, children) => {
-  return <div style={{ display: "flex", justifyContent: "center" }}>{>children}</div>
+  return <div style={{ display: "flex", justifyContent: "center" }}>{children}</div>
 }
