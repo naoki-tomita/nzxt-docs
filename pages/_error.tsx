3896e3a03f7dee5836ae2befcc5e802e5af00a91
kojiro.ueda <mxcpy608@hotmail.com>
2020-10-12 11:53:02 +0900

    Error

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -2,12 +2,14 @@ import { h, Component } from "nzxt/h";
 import { Content } from "../components/Content";
 import { Header } from "../components/Header";
 
-const Error: Component = () => {
+const Error: Component<{ error: Error }> = ({ error }) => {
   return (
     <div>
       <Header />
       <Content>
-        <h2>Unexpected error has occured.</h2>
+        <pre>
+          <code>{error.stack}</code>
+        </pre>
       </Content>
     </div>
   )
