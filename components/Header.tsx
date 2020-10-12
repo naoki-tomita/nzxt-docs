41216fd8eaeb48b3138d32a6bfb9d1ee426656cc
kojiro.ueda <mxcpy608@hotmail.com>
2020-10-12 10:56:33 +0900

    Use _document

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,6 @@ import { h, Component } from "zheleznaya";
 export const Header: Component = () => {
   return (
     <header>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"></link>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.21.0/themes/prism-okaidia.min.css"></link>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.21.0/components/prism-core.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.21.0/plugins/autoloader/prism-autoloader.min.js"></script>
       <h1 style={{ fontSize: "3.25em" }}><a style={{ textDecoration: "none", color: "#dddddd" }} href="/">nzxt documentation</a></h1>
     </header>
   )
---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import { h, Component } from "nzxt/h";
+
+const Document: Component = (_, children) => {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"></link>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.21.0/themes/prism-okaidia.min.css"></link>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.21.0/components/prism-core.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.21.0/plugins/autoloader/prism-autoloader.min.js"></script>
+        <title>Document</title>
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
+
+export default Document;
