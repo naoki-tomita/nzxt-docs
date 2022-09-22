77e879e060b991e162a0f7298d184d4eecafd0e5
naoki.tomita <naoki.tomita@uzabase.com>
2022-09-22 22:14:38 +0900

    improve multi arch image

--- env/docker/build_and_push.sh
+++ env/docker/build_and_push.sh
@@ -1,2 +1,1 @@
-docker buildx build ../../ -f Dockerfile --platform linux/arm64  -t ghcr.io/naoki-tomita/nzxt-docs:latest-arm --push
-docker buildx build ../../ -f Dockerfile --platform linux/amd64  -t ghcr.io/naoki-tomita/nzxt-docs:latest --push
+docker buildx build ../../ -f Dockerfile --platform linux/amd64,linux/arm64  -t ghcr.io/naoki-tomita/nzxt-docs:latest --push
