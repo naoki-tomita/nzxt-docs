c1875a849f8227faf009aed08f2de4a45a0e04ca
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2022-10-12 14:55:52 +0900

    Remove

--- app/index.js
+++
@@ -1,9 +0,0 @@
-const { command } = require("nzxt");
-
-const app = {
-  async main(req, res) {
-    const nzxt = await command("serve");
-    nzxt.onRequest(req, res);
-  }
-}
-module.exports = app;
