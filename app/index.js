6ce26f63026079639d03cf547d785582175b6acb
naoki.tomita <6903308+naoki-tomita@users.noreply.github.com>
2025-11-14 17:36:40 +0900

    WIP

--- app/index.js
+++ app/index.js
@@ -1,18 +1,17 @@
 const { create } = require("nzxt");
+// const { register } = require("esbuild-register/dist/node");
 const { readdir, readFile } = require("fs/promises");
 
-const app = {
-  async handle(req, res) {
-    console.log("hello world")
-    console.log(process.argv0, process.argv, process.env, process.execArgv, process.execPath)
-    console.log((await readFile(process.argv[1])).toString("utf-8"))
-    console.log((await readFile("/var/runtime/UserFunction.js")).toString("utf-8"))
-    console.log(process.cwd());
-    console.log(await readdir("./"));
-    console.log(await readdir("./pages"));
-    const app = await create();
-    app.handle(req, res);
-  }
-}
+// register({ tsconfigRaw: "./tsconfig.json" });
 
-module.exports = app;
+module.exports = async (req, res) => {
+  console.log("hello world")
+  console.log(process.argv0, process.argv, process.env, process.execArgv, process.execPath)
+  console.log((await readFile(process.argv[1])).toString("utf-8"))
+  console.log(process.cwd());
+  console.log(await readdir("./"));
+  console.log(await readdir("./pages"));
+  const app = await create();
+  app.handle(req, res);
+  res.status(200).json({ message: "Hello from Vercel Pure Handler!" });
+};
